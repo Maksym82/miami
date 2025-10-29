2880586d75b60c15fa58409dd46efe9eaa352dd2
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-10-29 12:10:11 +0200

    Update styles

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const cliderMain = new Swiper(".slider_main", {
+const sliderMain = new Swiper(".slider_main", {
   freeMode: true,
   centeredSlides: true,
   mousewheel: true,
@@ -14,4 +14,26 @@ const cliderMain = new Swiper(".slider_main", {
       spaceBetween: 60,
     },
   },
+});
+
+const sliderBg = new Swiper(".slider_bg", {
+  centeredSlides: true,
+  parallax: true,
+  spaceBetween: 60,
+  slidesPerView: 3.5,
+});
+
+sliderMain.controller.control = sliderBg;
+
+document.querySelectorAll(".slider__item").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    item.classList.toggle("opened");
+  });
+});
+
+let desc = document.querySelector(".description");
+sliderMain.on("slideChange", (e) => {
+  sliderMain.activeIndex > 0
+    ? desc.classList.add("hidden")
+    : desc.classList.remove("hidden");
 });
